5bec6ce5ecbdcc3140b4d4fff91f3024965a5c5b
SimonH <simonh@zuma.ai>
2022-09-30 11:28:17 +0100

    Add login-with-amazon-sdk.jar Add private funs to FlutterLoginWithAmazonPlugin.kt

--- android/src/main/kotlin/com/projexe/flutter_login_with_amazon/FlutterLoginWithAmazonPlugin.kt
+++ android/src/main/kotlin/com/projexe/flutter_login_with_amazon/FlutterLoginWithAmazonPlugin.kt
@@ -1,5 +1,7 @@
 package com.projexe.flutter_login_with_amazon
 
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -7,6 +9,70 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import org.json.JSONObject
+
+/**
+ * all responses must be on the main thread because of dart
+ * due to the amazon implementation using listeners its possible they come back on a different thread
+ * https://flutter.dev/docs/development/platform-integration/platform-channels#jumping-to-the-ui-thread-in-android
+ * **/
+private fun resultError(result: Result, authError: AuthError? = null){
+  Handler(Looper.getMainLooper()).post {
+    result.error("Error", authError?.message, null)
+  }
+}
+private fun resultSuccess(result: Result, success: Any?){
+  Handler(Looper.getMainLooper()).post {
+    result.success(success)
+  }
+}
+private fun resultAuthorized(result: Result, authorizedResult: AuthorizeResult?){
+  resultSuccess(result, authorizedResult?.let {
+    mapOf(
+      // these are null from amazon for some reason
+      "authorizationCode" to it.authorizationCode,
+      "clientId" to it.clientId,
+      "redirectUri" to it.redirectURI,
+      "accessToken" to it.accessToken,
+      "user" to it.user?.let { user ->
+        mapOf(
+          "userEmail" to user.userEmail,
+          "userId" to user.userId,
+          "userName" to user.userName,
+          "userPostalCode" to user.userPostalCode,
+          "userInfo" to user.userInfo
+        )
+      }
+    )
+  })
+}
+
+/**
+ * converts a map from dart to a JSONObject
+ * **/
+private fun jsonFromMap(map: Map<String,Any>): JSONObject = JSONObject().apply {
+  map.forEach{item ->
+    @Suppress("UNCHECKED_CAST")
+    when (item.value){
+      is Map<*,*> -> put(item.key, jsonFromMap(item.value as Map<String, Any>))
+      else -> put(item.key, item.value)
+    }
+  }
+}
+
+/**
+ * converts a map from dart to an amazon scope
+ * **/
+private fun createScopes(arguments: Map<String, Any>): Array<Scope> =
+  arguments.map { scope ->
+    @Suppress("UNCHECKED_CAST")
+    when(scope.value) {
+      is Map<*, *> ->
+        ScopeFactory.scopeNamed(scope.key, jsonFromMap(scope.value as Map<String, Any>))
+      else -> ScopeFactory.scopeNamed(scope.key)
+    }
+  }.toTypedArray()
+
 
 /** FlutterLoginWithAmazonPlugin */
 class FlutterLoginWithAmazonPlugin: FlutterPlugin, MethodCallHandler {
